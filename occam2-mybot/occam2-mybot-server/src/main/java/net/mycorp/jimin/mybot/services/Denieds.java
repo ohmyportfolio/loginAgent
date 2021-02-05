258122b1c0fdd5d5cfb4b8de228fc6aab88fb757
hurxxxx <hurxxxx@gmail.com>
2021-02-05 23:29:42 +0900

    외부 접근 이력 추가

---
+++ occam2-mybot/occam2-mybot-server/src/main/java/net/mycorp/jimin/mybot/services/Denieds.java
@@ -0,0 +1,19 @@
+package net.mycorp.jimin.mybot.services;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.mycorp.jimin.base.core.Configs;
+import net.mycorp.jimin.base.domain.OcContext;
+import net.mycorp.jimin.base.domain.OcMap;
+import net.mycorp.jimin.base.domain.OcResult;
+import net.mycorp.jimin.base.service.BaseService;
+
+@Service
+public class Denieds extends BaseService{
+	
+}
--- occam2-mybot/occam2-mybot-server/src/main/java/net/mycorp/jimin/mybot/services/Occupieds.java
+++ occam2-mybot/occam2-mybot-server/src/main/java/net/mycorp/jimin/mybot/services/Occupieds.java
@@ -3,6 +3,9 @@ package net.mycorp.jimin.mybot.services;
 import java.util.Date;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import net.mycorp.jimin.base.domain.OcContext;
@@ -13,6 +16,13 @@ import net.mycorp.jimin.base.util.Helper;
 @Service
 public class Occupieds extends BaseService{
 	
+	@Autowired
+	private Pcs pcs;
+	
+	@Autowired
+	private Denieds denieds;
+	
+	private static Logger log = LoggerFactory.getLogger(Sites.class);
 	
 	@Override
 	public void preInsert(OcContext ctx) {
@@ -24,16 +34,27 @@ public class Occupieds extends BaseService{
 	}
 	
 	
-	public void useSite(OcContext ctx) {
+	public String useSite(OcContext ctx) {
 		Map<String, Object> useInfo = ctx.getRow();
 		String ip = (String) useInfo.get("ip");
+		
+		useInfo.put("reg_date" , new Date());
 	
+		OcMap registeredPc = pcs.get(condition("ip" , ip));
+		if(registeredPc == null) {
+			log.info("denied IP : " + ip);
+			denieds.insert(useInfo);
+			return "false";
+		}
+		
 		//{ip=192.168.219.103, netflix=true, uflix=false, tving=false, wavve=false}
 		
 		OcMap userParams = new OcMap();
 		userParams.add("pc_ip", ip);
 
 		delete(userParams);
+		
+		return "true";
 		
 	}
 }
