8aa0fda11b18a4fbdc291ed5b6819b713b152791
hurxxxx <75428618+hurxxxx@users.noreply.github.com>
2021-08-04 23:15:46 +0900

    PC 정보에 PK 항목 추가

--- occam2-mybot/occam2-mybot-web/src/views/pages/pcs.vue
+++ occam2-mybot/occam2-mybot-web/src/views/pages/pcs.vue
@@ -2,10 +2,10 @@
   <div class="responsive-paddings">
     <oc-data-grid title="사이트 관리" resource="pcs" :has-form="false" :show-refresh="true" 
       :show-bulk-delete="true" :enableImport="true">
+      <oc-column data-field="id" caption="#" width="50" :visible="false"/>
       <oc-column data-field="store_id" caption="매장명" width="200" target-type="resource" type-id="stores" calculate-display-value="store__name"/>
       <oc-column data-field="ip" caption="IP 주소" width="200"/>
       <oc-column data-field="name" caption="자리번호" width="200"/>
-      
       <dx-filter-row :visible="true" apply-filter="onClick"/>
       <dx-filter-panel :visible="true" />
       <dx-search-panel :visible="true"/>
