fa508893b1694bcb543ee512b0807cc30582d8bf
hurxxxx <hurxxxx@gmail.com>
2022-03-04 11:36:47 +0900

    디즈니 플러스 프로필 수정 제한 로그인/패스워드 버튼 xpath 변경

--- LoginAgentClient/Launcher.cs
+++ LoginAgentClient/Launcher.cs
@@ -162,7 +162,7 @@ namespace LoginAgent
                         Console.WriteLine("CH Url for '" + process.MainWindowTitle + "' is " + url);
                         if (url.Contains("netflix.com/YourAccount") || url.Contains("uflix.co.kr/uws/web/mine/userInfo")
                             || url.Contains("member.wavve.com/me") || url.Contains("tving.com/my/main") || url.Contains("tving.com/my/watch")
-                            || url.Contains("netflix.com/ManageProfiles")
+                            || url.Contains("netflix.com/ManageProfiles") || url.Contains("edit-profiles")
 
                             )
                         {
