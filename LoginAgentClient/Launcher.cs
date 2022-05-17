5fe0c2dc7ac4a4f123b4ca72069c62bcf363eb0b
hurxxxx <hurxxxx@gmail.com>
2022-05-17 22:15:27 +0900

    디즈니 추가

--- LoginAgentClient/Launcher.cs
+++ LoginAgentClient/Launcher.cs
@@ -167,6 +167,9 @@ namespace LoginAgent
                             || url.Contains("netflix.com/ManageProfiles") || url.Contains("edit-profiles")
                             || url.Contains("profiles/manage") || url.Contains("profilesForEdit") || url.Contains("profileForEdit")
                             || url.Contains("wavve.com/my") || url.Contains("wavve.com/voucher") || url.Contains("membership/tving")
+                            || url.Contains("app-settings") || url.Contains("/account") || url.Contains("help.disneyplus.com")  //disney
+
+                            
 
                             )
                         {
--- LoginAgentClient/Main.Designer.cs
+++ LoginAgentClient/Main.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.metroButton1 = new MetroFramework.Controls.MetroButton();
-            this.labelNetflix = new System.Windows.Forms.Label();
-            this.labelTving = new System.Windows.Forms.Label();
-            this.labelWavve = new System.Windows.Forms.Label();
             this.disneyBtn = new MetroFramework.Controls.MetroButton();
             this.metroButton2 = new MetroFramework.Controls.MetroButton();
             this.tvingBtn = new MetroFramework.Controls.MetroButton();
             this.netFlixBtn = new MetroFramework.Controls.MetroButton();
+            this.labelNetflix = new System.Windows.Forms.Label();
+            this.labelTving = new System.Windows.Forms.Label();
+            this.labelWavve = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // metroButton1
@@ -43,7 +43,7 @@
             this.metroButton1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.metroButton1.FontSize = MetroFramework.MetroButtonSize.Tall;
             this.metroButton1.Highlight = true;
-            this.metroButton1.Location = new System.Drawing.Point(251, 484);
+            this.metroButton1.Location = new System.Drawing.Point(355, 484);
             this.metroButton1.Name = "metroButton1";
             this.metroButton1.Size = new System.Drawing.Size(185, 55);
             this.metroButton1.Style = MetroFramework.MetroColorStyle.Black;
@@ -51,36 +51,6 @@
             this.metroButton1.Text = "영상보기 종료";
             this.metroButton1.UseSelectable = true;
             this.metroButton1.Click += new System.EventHandler(this.CloseClick);
-            // 
-            // labelNetflix
-            // 
-            this.labelNetflix.AutoSize = true;
-            this.labelNetflix.Location = new System.Drawing.Point(93, 438);
-            this.labelNetflix.Name = "labelNetflix";
-            this.labelNetflix.Size = new System.Drawing.Size(31, 12);
-            this.labelNetflix.TabIndex = 3;
-            this.labelNetflix.Text = "0 / 0";
-            this.labelNetflix.Visible = false;
-            // 
-            // labelTving
-            // 
-            this.labelTving.AutoSize = true;
-            this.labelTving.Location = new System.Drawing.Point(326, 439);
-            this.labelTving.Name = "labelTving";
-            this.labelTving.Size = new System.Drawing.Size(31, 12);
-            this.labelTving.TabIndex = 3;
-            this.labelTving.Text = "0 / 0";
-            this.labelTving.Visible = false;
-            // 
-            // labelWavve
-            // 
-            this.labelWavve.AutoSize = true;
-            this.labelWavve.Location = new System.Drawing.Point(552, 439);
-            this.labelWavve.Name = "labelWavve";
-            this.labelWavve.Size = new System.Drawing.Size(31, 12);
-            this.labelWavve.TabIndex = 3;
-            this.labelWavve.Text = "0 / 0";
-            this.labelWavve.Visible = false;
             // 
             // disneyBtn
             // 
@@ -90,7 +60,6 @@
             this.disneyBtn.Size = new System.Drawing.Size(198, 399);
             this.disneyBtn.TabIndex = 4;
             this.disneyBtn.UseSelectable = true;
-            this.disneyBtn.Visible = false;
             this.disneyBtn.Click += new System.EventHandler(this.disneyBtn_Click);
             // 
             // metroButton2
@@ -123,12 +92,42 @@
             this.netFlixBtn.UseSelectable = true;
             this.netFlixBtn.Click += new System.EventHandler(this.NetFlixBtnClick);
             // 
+            // labelNetflix
+            // 
+            this.labelNetflix.AutoSize = true;
+            this.labelNetflix.Location = new System.Drawing.Point(93, 438);
+            this.labelNetflix.Name = "labelNetflix";
+            this.labelNetflix.Size = new System.Drawing.Size(31, 12);
+            this.labelNetflix.TabIndex = 3;
+            this.labelNetflix.Text = "0 / 0";
+            this.labelNetflix.Visible = false;
+            // 
+            // labelTving
+            // 
+            this.labelTving.AutoSize = true;
+            this.labelTving.Location = new System.Drawing.Point(326, 439);
+            this.labelTving.Name = "labelTving";
+            this.labelTving.Size = new System.Drawing.Size(31, 12);
+            this.labelTving.TabIndex = 3;
+            this.labelTving.Text = "0 / 0";
+            this.labelTving.Visible = false;
+            // 
+            // labelWavve
+            // 
+            this.labelWavve.AutoSize = true;
+            this.labelWavve.Location = new System.Drawing.Point(552, 439);
+            this.labelWavve.Name = "labelWavve";
+            this.labelWavve.Size = new System.Drawing.Size(31, 12);
+            this.labelWavve.TabIndex = 3;
+            this.labelWavve.Text = "0 / 0";
+            this.labelWavve.Visible = false;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.metroButton1;
-            this.ClientSize = new System.Drawing.Size(688, 551);
+            this.ClientSize = new System.Drawing.Size(892, 551);
             this.Controls.Add(this.disneyBtn);
             this.Controls.Add(this.labelWavve);
             this.Controls.Add(this.labelTving);
@@ -152,10 +151,10 @@
         private MetroFramework.Controls.MetroButton tvingBtn;
         private MetroFramework.Controls.MetroButton metroButton1;
         private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton disneyBtn;
         private System.Windows.Forms.Label labelNetflix;
         private System.Windows.Forms.Label labelTving;
         private System.Windows.Forms.Label labelWavve;
-        private MetroFramework.Controls.MetroButton disneyBtn;
     }
 }
 
